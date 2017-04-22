e6fcc4a9734d1f18248a157b8c263923952ee616
Evgeniy <vasenev.ea@mail.ru>
2017-04-23 00:13:57 +0300

    add structure

---
+++ Bot.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Telegram.Bot;
+using System.Threading;
+
+namespace telegrambot
+{
+    public class Bot
+    {
+        private Api bot;
+        private bool runnable = false;
+
+        public Bot(string token)
+        {
+
+        }
+
+        public void start()
+        {
+            var Bot = new Telegram.Bot.Api("your API access Token");
+            var me = Bot.GetMe();
+            loop();
+        }
+
+        public void stop()
+        {
+
+        }
+
+
+        private void loop()
+        {
+            while (runnable)
+            {
+
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Service.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telegrambot
+{
+    public static class Service
+    {
+        public static string readConfig()
+        {
+            return null;
+        }
+    }
+}
